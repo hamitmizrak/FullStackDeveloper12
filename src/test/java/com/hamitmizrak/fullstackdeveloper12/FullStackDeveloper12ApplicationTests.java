23905f142d9feba45ff8c4808fcf3b644a802c90
HamitMizrak <hamitmizrak@gmail.com>
2023-11-12 16:45:05 +0300

    Project Template

--- src/test/java/com/hamitmizrak/fullstackdeveloper12/FullStackDeveloper12ApplicationTests.java
+++ src/test/java/com/hamitmizrak/fullstackdeveloper12/FullStackDeveloper12ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FullStackDeveloper12ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
