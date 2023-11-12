c9d6b042210625c38bedecdcddbb89d7560ad862
HamitMizrak <hamitmizrak@gmail.com>
2023-11-12 16:12:43 +0300

    Initial commit

---
+++ src/main/java/com/hamitmizrak/fullstackdeveloper12/FullStackDeveloper12Application.java
@@ -0,0 +1,13 @@
+package com.hamitmizrak.fullstackdeveloper12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackDeveloper12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackDeveloper12Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/hamitmizrak/fullstackdeveloper12/FullStackDeveloper12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamitmizrak.fullstackdeveloper12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackDeveloper12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
