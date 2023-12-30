962beaa59aecfabb8dd5d1606bd60d90b8306c90
Hamit Mizrak <hamitmizrak@gmail.com>
2023-12-30 13:36:49 +0300

    Header, Main,Footer

--- blog/src/components/BlogHeader.jsx
+++ blog/src/components/BlogHeader.jsx
@@ -17,6 +17,7 @@ import {
 import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
 import { withTranslation } from 'react-i18next'
 
+// Navbar
 const products = [
   { name: 'Analytics', description: 'Get a better understanding of your traffic', href: '#', icon: ChartPieIcon },
   { name: 'Engagement', description: 'Speak directly to your customers', href: '#', icon: CursorArrowRaysIcon },
@@ -40,13 +41,15 @@ function BlogHeader() {
 // bg-sky-100
   return (
     <header className="bg-blue-300 text-white">
+
       <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
-            <span className="sr-only">Your Company</span>
+            <span className="sr-only">Blog</span>
             <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
           </a>
         </div>
+
         <div className="flex lg:hidden">
           <button
             type="button"
@@ -57,6 +60,7 @@ function BlogHeader() {
             <Bars3Icon className="h-6 w-6" aria-hidden="true" />
           </button>
         </div>
+
         <Popover.Group className="hidden lg:flex lg:gap-x-12">
           <Popover className="relative">
             <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
@@ -93,6 +97,7 @@ function BlogHeader() {
                     </div>
                   ))}
                 </div>
+
                 <div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
                   {callsToAction.map((item) => (
                     <a
@@ -125,6 +130,8 @@ function BlogHeader() {
           </a>
         </div>
       </nav>
+
+
       <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
         <div className="fixed inset-0 z-10" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
@@ -146,6 +153,7 @@ function BlogHeader() {
               <XMarkIcon className="h-6 w-6" aria-hidden="true" />
             </button>
           </div>
+
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
@@ -174,6 +182,7 @@ function BlogHeader() {
                     </>
                   )}
                 </Disclosure>
+
                 <a
                   href="#"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
@@ -193,6 +202,7 @@ function BlogHeader() {
                   Company
                 </a>
               </div>
+
               <div className="py-6">
                 <a
                   href="#"
